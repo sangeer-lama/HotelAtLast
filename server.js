2fbd9d5e4ca908d04cff8735a8bad98a216d4732
sangeer negi <sangeer.negi@gmail.com>
2022-07-20 11:30:44 +0545

    for deploy frontend

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const dbConfig = require('./db')
-
-const roomsRoute = require('./routes/roomsRoute')
-const usersRoute = require('./routes/usersRoute')
-const bookingsRoute = require('./routes/bookingsRoute')
-app.use(express.json())
-
-app.use('/api/rooms' , roomsRoute);
-app.use('/api/users',usersRoute);
-app.use('/api/bookings',bookingsRoute); 
+const dbConfig = require("./db");
+const path = require("path");
+const roomsRoute = require("./routes/roomsRoute");
+const usersRoute = require("./routes/usersRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+app.use(express.json());
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/bookings", bookingsRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,() => console.log(`node server started using nodemon`));
+app.listen(port, () => console.log(`node server started using nodemon`));
